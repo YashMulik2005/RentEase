a66a98cd78ea217182d7609fcd76ab1afb3e8dda
Shree Takalkar <shreetakalkar087@gmail.com>
2025-01-27 22:11:51 +0530

    routes changes

--- RentEase-backend/index.js
+++ RentEase-backend/index.js
@@ -2,22 +2,17 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes');
 
-const { signup, login } = require('./controllers/authUser');
-const {ownerSignup,ownerLogin}=require('./controllers/authOwner');
 const app = express();
 app.use(bodyParser.json());
-
 
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch((err) => console.log(err));
 
-app.post('/usersignup', signup);
-app.post('/userlogin', login);
-app.post('/ownersignup', ownerSignup);
-app.post('/ownerlogin', ownerLogin);
+app.use(authRoutes);
 
-const PORT =8080;
-app.listen(PORT, () => console.log(`Server running on port`));
+const PORT = 8080;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- RentEase-backend/routes/authRoutes.js
+++ RentEase-backend/routes/authRoutes.js
@@ -1,9 +1,12 @@
 const express = require('express');
-const { signup, login } = require('../controllers/authController');
+const { signup, login } = require('../controllers/authUser');
+const { ownerSignup, ownerLogin } = require('../controllers/authOwner');
 
 const router = express.Router();
 
-router.post('/signup', signup);
-router.post('/login', login);
+router.post('/usersignup', signup);
+router.post('/userlogin', login);
+router.post('/ownersignup', ownerSignup);
+router.post('/ownerlogin', ownerLogin);
 
 module.exports = router;
